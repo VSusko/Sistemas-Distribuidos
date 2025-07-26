a5850a165b35ba4e2682dd457add4cbf99ef7fe5
Gabriel Carlos Silva <gabrielcsilva413@hotmail.com>
2025-07-26 11:26:54 -0300

    Update app.py
    Xuxei um log no trem pra ele ficar doido

--- Sistemas-Distribuidos-TP2/critical_server/app.py
+++ Sistemas-Distribuidos-TP2/critical_server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify  # Framework web e utilitários para requisições/respostas
+import time                                # Para marcar timestamps e simular uso da região crítica
 
 # Inicializa a aplicação Flask
 app = Flask(__name__)  
@@ -8,6 +9,14 @@ def critical():
     client_data = request.json
     client_name = client_data.get("node")            # Obtendo nome do cliente
     client_timestamp = client_data.get("timestamp")  # Obtendo timestamp do cliente
+    current_time = time.time()
+
+    with open('log.txt', 'a') as file:
+        file.write("\n ################# \n")
+        file.write("\nNode: {client_name}")
+        file.write("\nTimestamp: {client_timestamp}")
+        file.write("\nArrived at: {current_time}")
+        file.write("\n ################# \n")
 
     print(f"🤑 Acesso à região crítica. Nome do cliente: {client_name}, Timestamp: {client_timestamp}", flush=True)
     return "", 200
